1de9566a44322a60aeb5d0d4059bb64508c948f1
Julian Mierzejewski <j3mierz@gmail.com>
2024-04-11 16:53:53 +0200

    flask examples

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from datetime import datetime
 from random import randint
+
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/welcome")
 def hello_world():
     return "użytkowniku user"
 
@@ -29,9 +30,18 @@ def count(a, b):
     return str(a + b)
 
 
+@app.route("/kontakt", methods=['GET', 'POST'])
+def formularz():
+    if request.method == 'POST':
+        first_name_1 = request.form.get('firstname')
+        return render_template("confirmation.html", first_name=first_name_1)
+    return render_template("form.html")
+
+
 @app.route("/losuj")
 def dice():
     return f'{randint(0, 9)}, {randint(0, 9)}, {randint(0, 9)}'
+
 
 @app.route("/lotek")
 def lotto():
@@ -40,5 +50,34 @@ def lotto():
         a = randint(0, 50)
         if a not in result:
             result.append(str(a))
-    return ", ".join(result)
+    return "-".join(result)
 
+
+operations = {
+    "+": lambda x, y: x + y,
+    "-": lambda x, y: x - y,
+    "*": lambda x, y: x * y,
+    "/": lambda x, y: x / y,
+}
+
+
+@app.route("/kalkulator", methods=['GET', 'POST'])
+def kalkulator():
+    result = ''
+    if request.method == 'POST':
+        operation = request.form.get('operation')
+        try:
+            d1 = float(request.form.get('digit_1'))
+            d2 = float(request.form.get('digit_2'))
+        except ValueError:
+            return render_template("calculator_form.html", message="podaj poprawne liczby")
+
+        result = operations.get(operation)(d1, d2)
+
+    return render_template("calculator_form.html", result=result)
+
+@app.route("/method", methods=['GET', 'POST'])
+def type_of_comm():
+    if request.method == 'POST':
+        return str('ty Post')
+    return str('ty GET')
